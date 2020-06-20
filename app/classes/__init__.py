9a5a7f42413a096549cb35c4c2537eb49acc9896
jak <jak@dev.cymru>
2020-06-20 20:23:16 +0100

    this works, sets the stage for metaclass override behaviour

--- app/classes/__init__.py
+++ app/classes/__init__.py
@@ -1,6 +1,20 @@
-from .session_resource import SessionResource
-from .user import User
-from .jmap import JMAP
-from .mailbox import Mailbox
-from .core import Core
-from .result_reference import ResultReference
+from importlib import import_module
+
+cs = {'session_resource':'SessionResource',
+      'user':'User',
+      'jmap':'JMAP',
+      'mailbox':'Mailbox',
+      'core':'Core',
+      'result_reference':'ResultReference'
+     }
+
+for k,v in cs.items():
+    b  = import_module("classes."+k)
+    ba = getattr(b, v)
+
+    vars()[v] = ba
+
+    #ov = import_module("objs.override."+k)
+    #ova = getattr(ov, v)
+    #vars()[v] = type(k, (ova, ba), {})
+
